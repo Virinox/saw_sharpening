0c459b1c9026dc89a9169c64756f64046a6ce341
Virinox <vincecmao@gmail.com>
2019-04-18 21:55:13 -0400

    Create Exercise_1.py
    Code for exercise 1.

---
+++ Exercise_1.py
@@ -0,0 +1,26 @@
+# Exercise 1
+# written by: Vince Mao
+# last modified: 2019.4.18
+# Description:
+# Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+# between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+#
+# Hints:
+# Consider use range(#begin, #end) method
+
+# Pseudocode:
+# 1. Initialize an empty list
+# 2. Loop from 2000 to 3201
+# 3. Check if the number is divisible by 7 but not 5
+# 4. If true, append to the list
+# 5. Else, ignore
+# 6. Return the list
+
+result = []
+
+for i in range(2000, 3201):
+    if (i % 7 == 0) and (i % 5 != 0):
+        result.append(i)
+
+print result
