188858ed21d1349fe64cf533cb769c5ed2007f09
Virinox <vincecmao@gmail.com>
2019-05-02 16:58:37 -0400

    Create Exercise_26.py
    Added code for exercise 26.

---
+++ Exercise_26.py
@@ -0,0 +1,20 @@
+# Exercise 26
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.2
+# Description:
+# Write a function that returns the sum of two numbers.
+#
+# Hint:
+# Define a function with two numbers as arguments. You can compute the sum in the function and return the result.
+#
+# Pseudocode:
+# 1. Define function that takes two integer values.
+# 2. Return the sum.
+
+
+def add_two_nums(a, b):
+    return a + b
+
+
+print add_two_nums(3, 5)
