fd7aeb2831ad77960f5990b5cad20f7b58d40230
Virinox <vincecmao@gmail.com>
2019-05-02 16:51:11 -0400

    Create Exercise_25.py
    Added code for exercise 25.

---
+++ Exercise_25.py
@@ -0,0 +1,27 @@
+# Exercise 25
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.2
+# Description:
+# Instantiate a class that has a class parameter and the same instance parameter.
+#
+# Hint:
+# Define an instance parameter by adding it in the __init__ method.
+# You can initiate an object with a constructor or set the value later.
+#
+# Pseudocode:
+# 1. Instantiate a class.
+# 2. Instantiate the __init__ method.
+# 3. Assign a parameter in the method.
+
+
+class Test:
+    def __init__(self):
+        self.value = 0
+
+    def generate(self):
+        return self.value
+
+
+test = Test()
+print test.generate()
