a89a274bef27818d58fc6c7a33330a6c750d564d
Virinox <vincecmao@gmail.com>
2019-05-02 17:09:35 -0400

    Create Exercise_29.py
    Added code for exercise 29.

---
+++ Exercise_29.py
@@ -0,0 +1,22 @@
+# Exercise 29
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.2
+# Description:
+# Define a function that takes two strings and returns the concatenated result.
+#
+# Hint:
+# Use "+" to add strings together.
+#
+# Pseudocode:
+# 1. Define function that takes two string and returns the concatenated result.
+
+
+def combine_two_strings(a, b):
+    if str(a) and str(b):
+        return a + b
+    else:
+        return "Two strings are needed!"
+
+
+print combine_two_strings("4", "6")
