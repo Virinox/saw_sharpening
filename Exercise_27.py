20136806780dc336a01ddfabd93f797f46fbf708
Virinox <vincecmao@gmail.com>
2019-05-02 17:02:20 -0400

    Create Exercise_27.py
    Added code for exercise 27.

---
+++ Exercise_27.py
@@ -0,0 +1,20 @@
+# Exercise 27
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.2
+# Description:
+# Write a function that converts any integer to a string and prints it.
+#
+# Hint:
+# Use the str() method.
+#
+# Pseudocode:
+# 1. Define function that takes an integer value and converts it to a string before printing it.
+
+
+def int_to_str(a):
+    return str(a)
+
+
+print int_to_str(3)
+print int_to_str(4) * 3
