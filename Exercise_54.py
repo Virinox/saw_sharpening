f639273a7f3447a72df584b82e494c98aa4d8136
Virinox <vincecmao@gmail.com>
2019-05-08 15:33:54 -0400

    Create Exercise_54.py

---
+++ Exercise_54.py
@@ -0,0 +1,15 @@
+# Exercise 54
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.8
+# Description:
+# Please raise a RuntimeError exception.
+#
+# Hint:
+# Use raise() to raise an exception.
+#
+# Pseudocode:
+# 1. Invoke raise RuntimeError("something").
+
+
+raise RuntimeError("Something evil comes this way...")
