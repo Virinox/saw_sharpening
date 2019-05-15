94d5e7ae8aca3883a4337da6f2619dbcb68abc83
Virinox <vincecmao@gmail.com>
2019-05-15 00:22:22 -0400

    Create Exercise_60.py

---
+++ Exercise_60.py
@@ -0,0 +1,15 @@
+# Exercise 60
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.14
+# Description:
+# Print the string "hello world" in unicode.
+#
+# Hint:
+# Use the u'strings' format to define a unicode string.
+#
+# Pseudocode:
+# 1. Print "hello world" in the unicode string format.
+
+
+print u'hello world'
