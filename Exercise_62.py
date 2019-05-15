f9cdc532fefce76ae4c83e77d31d194c79affd63
Virinox <vincecmao@gmail.com>
2019-05-15 00:23:40 -0400

    Create Exercise_62.py

---
+++ Exercise_62.py
@@ -0,0 +1,9 @@
+# Exercise 62
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.5.14
+# Description:
+# Write a special comment to indicate whether a Python source code file is in unicode.
+#
+# -*- This source code is in utf-8 format. -*-
+
