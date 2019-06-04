15457c156127e34e28bf2c6ea53b6105d4e5e2c1
Virinox <vincecmao@gmail.com>
2019-06-03 23:36:54 -0400

    Create Exercise_79.py

---
+++ Exercise_79.py
@@ -0,0 +1,22 @@
+# Exercise 79
+# Level 1
+# written by: Vince Mao
+# last modified: 2019.6.3
+# Description:
+# Please write a program to randomly print a integer number between 7 and 15 inclusive.
+#
+# Hint:
+# Use the random.randrange() method to generate a random integer in a given range.
+#
+# Pseudocode:
+# 1. Print a randomly generated number between 7 and 15
+#    using the random.randrange() method.
+
+
+import random
+
+print random.randrange(7, 16)
+print random.randrange(7, 16)
+print random.randrange(7, 16)
+print random.randrange(7, 16)
+print random.randrange(7, 16)
