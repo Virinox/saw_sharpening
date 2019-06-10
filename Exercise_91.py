2fa44882fa2ecf1b6d017e6019d1b51cc34aa100
Virinox <vincecmao@gmail.com>
2019-06-09 22:27:04 -0400

    Update Exercise_91.py
    Updating code with order preserved.

--- Exercise_91.py
+++ Exercise_91.py
@@ -4,7 +4,8 @@
 # last modified: 2019.6.9
 # Description:
 # Given the list [12,24,35,24,88,120,155,88,120,155], write a program
-# to create a list without duplicate elements.
+# to create a list without duplicate elements. Ensure the order of the
+# list is preserved.
 #
 # Hint:
 # Use set() to create list with no duplicates.
@@ -12,7 +13,16 @@
 # Pseudocode:
 # 1. Initialize the list.
 # 2. Print the result using set().
+# 3. Use this set on the original list to generate the new list.
 
 test = [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
-result = set(test)
-print list(result)
+check = list(set(test))
+result = []
+
+for i in range(len(test)):
+    if test[i] in check and test[i] not in result:
+        result.append(test[i])
+        
+print check
+print result
+
